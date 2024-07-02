eb40f99351232b4da064b16d6dfd6e70b3ec8aff
ChadiSle <csleiman21@gmail.com>
2024-07-02 18:11:20 -0400

    Add files via upload

---
+++ ConfigurationManagement/DeleteSites.py
@@ -0,0 +1,51 @@
+#DeleteSites
+import requests
+import json
+import csv
+
+# Set your API credentials
+Wex_Health_headers = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "x-api-id": "",
+    "x-api-key": ""
+    
+}
+Wex_Inc_headers = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "x-api-id": "",
+    "x-api-key": ""
+    
+}
+
+headers = input("Which Imperva Tenant are you searching? Wex Health/Wex Inc? :""\n")
+
+if headers == "Wex Health" or headers == "wex health" or headers == "Wex health" or headers == "wex Health" or headers == "Wex Helth":
+    headers = Wex_Health_headers
+
+elif headers == "Wex Inc" or headers == "wex inc" or headers == "Wex inc" or headers == "Wex Corp":
+    headers = Wex_Inc_headers
+
+
+site_ids = []
+kill_input = ""
+print("Please provide the site ids of the sites you wish to delete :" "\n")
+
+while True:
+    line = input()
+    if line == kill_input:
+        break
+    site_ids.extend(line.strip().split(","))
+
+print(site_ids)
+
+for site_id in site_ids:
+
+    api_url = f"https://my.imperva.com/api/prov/v1/sites/delete?site_id={site_id}&ignore_grace_period=true"
+
+    response = requests.post(api_url, headers=headers, verify=False)
+    print(response.status_code)
+    print(f"Site ID {site_id} has been deleted")
