ea52152696b3c3789ee921e8a35eebcc469ba722
ChadiSle <csleiman21@gmail.com>
2024-07-02 17:17:32 -0400

    Update ModifyOriginServer.py

--- ConfigurationManagement/ModifyOriginServer.py
+++ ConfigurationManagement/ModifyOriginServer.py
@@ -1,16 +1,35 @@
-import requests
+# In order to use this you will have to gather the Server IDs for the sites you wish to change the origin server for. You can do this with the ListServerIDs script in the InfoGrabbing folder of this Repository
 
+import requests
+#The below libaries allow for the creation of a custom HTTP Adapter which I have found necessary working from certain environment and versions of python. I believe the "Unsafe Legacy Renegotiation Disabled" error is due to a CVE imperva may not be privy too regarding their latest SSL certificates on their API servers. Python has recently removed those unsecure certs from their ssl library. I suggest trying to run this script normally with the requests library before proceeding to disable ssl with this custom adapter if you get an ssl error.
+from requests import Session
+from requests import adapters
+from urllib3 import poolmanager
+from ssl import create_default_context, Purpose, CERT_NONE
+
+class CustomHttpAdapter (adapters.HTTPAdapter):
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+
+def ssl_supressed_session():
+    ctx = create_default_context(Purpose.SERVER_AUTH)
+    # to bypass verification after accepting Legacy connections
+    ctx.check_hostname = False
+    ctx.verify_mode = CERT_NONE
+    # accepting legacy connections
+    ctx.options |= 0x4    
+    session = Session()
+    session.mount('https://', CustomHttpAdapter(ctx))
+    return session
 
 # Set your API credentials
-Wex_Health_headers = {
-    "Content-Type": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "x-api-id": "",
-    "x-api-key": ""
-    
-}
-Wex_Inc_headers = {
+Imperva_Headers_Account1 = {
     "Content-Type": "application/json",
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
@@ -19,14 +38,25 @@ Wex_Inc_headers = {
     
 }
 
-headers = input("Which Imperva Tenant are you searching? Wex Health/Wex Inc? :""\n")
+Imperva_Headers_Account2 = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "x-api-id": "",
+    "x-api-key": ""
+    
+}
 
-if headers == "Wex Health" or headers == "wex health" or headers == "Wex health" or headers == "wex Health" or headers == "Wex Helth":
-    headers = Wex_Health_headers
+# Adjust the header variable names to reflect the name of the accounts you will most commonly be working on and the credentials you will need for them.
 
-elif headers == "Wex Inc" or headers == "wex inc" or headers == "Wex inc" or headers == "Wex Corp":
-    headers = Wex_Inc_headers
+headers = input("Which Imperva Tenant are you working with? :""\n")
 
+# Giving a few variations of common typos will help mitigate errors and breaks when running the script, eg lowercase, missed last letter etc.
+if headers == "Imperva_Headers_Account1" or headers == "imperva_headers_account1":
+    headers = Imperva_Headers_Account1
+
+elif headers == "Imperva_Headers_Account2" or headers == "imperva_headers_account2":
+    headers = Imperva_Headers_Account2
 
 server_ids = []
 kill_input = ""
@@ -45,7 +75,7 @@ for server_id in server_ids:
     
     api_url = (f"https://my.imperva.com/api/prov/v1/sites/dataCenters/servers/edit?server_id={server_id}&server_address={appgw_cname}&is_enabled=true&is_standby=false")
 
-    response = requests.post(api_url,headers=headers,verify=False)
+    response = ssl_supressed_session().post(api_url,headers=headers,verify=False)
 
     print(f"Server ID {server_id} response status code {response.status_code}")
 
