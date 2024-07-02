7f537b51fbc86aadd259d559c2b2faa11be927bd
ChadiSle <csleiman21@gmail.com>
2024-07-02 17:55:44 -0400

    Update GetSiteIDs.py

--- InfoGrabbing/GetSiteIDs.py
+++ InfoGrabbing/GetSiteIDs.py
@@ -1,8 +1,7 @@
 import requests
 import re
 import pandas
-import asyncio
-
+#The below libaries allow for the creation of a custom HTTP Adapter which I have found necessary working from certain environment and versions of python. I believe the "Unsafe Legacy Renegotiation Disabled" error is due to a CVE imperva may not be privy too regarding their latest SSL certificates on their API servers. Python has recently removed those unsecure certs from their ssl library. I suggest trying to run this script normally with the requests library before proceeding to disable ssl with this custom adapter if you get an ssl error.
 from requests import Session
 from requests import adapters
 from urllib3 import poolmanager
@@ -29,17 +28,8 @@ def ssl_supressed_session():
     session.mount('https://', CustomHttpAdapter(ctx))
     return session
 
-    
 # Set your API credentials
-Wex_Health_headers = {
-    "Content-Type": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "x-api-id": "",
-    "x-api-key": ""
-    
-}
-Wex_Inc_headers = {
+Imperva_Headers_Account1 = {
     "Content-Type": "application/json",
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
@@ -48,26 +38,42 @@ Wex_Inc_headers = {
     
 }
 
-headers = input("Which Imperva Tenant are you searching? Wex Health/Wex Inc? :""\n")
+Imperva_Headers_Account2 = {
+    "Content-Type": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "x-api-id": "",
+    "x-api-key": ""
+    
+}
 
-if headers == "Wex Health" or headers == "wex health" or headers == "Wex health" or headers == "wex Health" or headers == "Wex Helth":
-    headers = Wex_Health_headers
+# Adjust the header variable names to reflect the name of the accounts you will most commonly be working on and the credentials you will need for them.
 
-elif headers == "Wex Inc" or headers == "wex inc" or headers == "Wex inc" or headers == "Wex Corp":
-    headers = Wex_Inc_headers
+headers = input("Which Imperva Tenant are you working with? :""\n")
+
+# Giving a few variations of common typos will help mitigate errors and breaks when running the script, eg lowercase, missed last letter etc.
+if headers == "Imperva_Headers_Account1" or headers == "imperva_headers_account1":
+    headers = Imperva_Headers_Account1
+
+elif headers == "Imperva_Headers_Account2" or headers == "imperva_headers_account2":
+    headers = Imperva_Headers_Account2
 
 #You will need to add subaccount IDs if new subaccounts are added to a tenant, and adjust the rest of the script accordingly 
-Wex_Health_ID = 1745073
-Partner_Vanities_ID = 2014828
-Wex_Inc_ID = 884886
 
-AccountID = input("Is this for the Wex Health or Partner Vanities account ?: ""\n")
-if AccountID == "Wex Health" or AccountID == "wex health" or AccountID == "Wex health" or AccountID == "wex Health":
-    AccountID = Wex_Health_ID
-elif AccountID == "Partner Vanities" or AccountID == "partner vanities" or AccountID == "Partner vanities" or AccountID == "partner Vanities":
-    AccountID = Partner_Vanities_ID
-else:
-    AccountID = Wex_Inc_ID
+account_id = input("Which account or subaccount will you be searching? Account1/Account2/Subaccount1ofAccount1? :""\n")
+
+#Add Parent and Subaccount IDs here to easily differentiate and pivot where you want to search for these site_ids, add some room for error when specifying as above with the header names
+if account_id == "Account1" or account_id == "account1":
+    account_id = ""
+    headers = Imperva_Headers_Account1
+
+elif account_id == "Account1_Subaccount1" or account_id == "account1_subaccount1":
+    account_id = ""
+    headers = Imperva_Headers_Account1
+
+elif account_id == "Account2":
+    account_id = ""
+    headers = Imperva_Headers_Account2
 
 target_domains = []
 kill_input = ""
@@ -82,13 +88,10 @@ while True:
 print(target_domains)
 domain_to_site_id = []
 matching_site_ids = []
-domain_site_data = []
-
-
 
 for target_domain in target_domains:
 
-    api_url = (f"https://api.imperva.com/sites-mgmt/v3/sites?names={target_domain}&siteTypes=&caid={AccountID}")
+    api_url = (f"https://api.imperva.com/sites-mgmt/v3/sites?names={target_domain}&siteTypes=&caid={accountID}")
 
     response = ssl_supressed_session().get(api_url, headers=headers)
     #response = requests.get(api_url, headers=headers, verify=False)
@@ -111,7 +114,7 @@ for target_domain in target_domains:
 
     
 data_frame = pandas.DataFrame(domain_to_site_id)
-excel_file_path = 'domain_to_site_id.xlsx'
+excel_file_path = 'Domain_to_Site_id.xlsx'
 data_frame.to_excel(excel_file_path, index=False)
 
 print(f"Excel file '{excel_file_path} created successfully.")
