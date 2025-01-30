32ca842218f7de9d7bbb09901c633e3f4e4a1092
ChadiSle <csleiman21@gmail.com>
2025-01-30 15:09:06 -0500

    Update ModifyOriginServer.py

--- ConfigurationManagement/ModifyOriginServer.py
+++ ConfigurationManagement/ModifyOriginServer.py
@@ -1,7 +1,7 @@
 # In order to use this you will have to gather the Server IDs for the sites you wish to change the origin server for. You can do this with the ListServerIDs script in the InfoGrabbing folder of this Repository
 
 import requests
-#The below libaries allow for the creation of a custom HTTP Adapter which I have found necessary working from certain environment and versions of python. I believe the "Unsafe Legacy Renegotiation Disabled" error is due to a CVE imperva may not be privy too regarding their latest SSL certificates on their API servers. Python has recently removed those unsecure certs from their ssl library. I suggest trying to run this script normally with the requests library before proceeding to disable ssl with this custom adapter if you get an ssl error.
+#The below libaries allow for the creation of a custom HTTP Adapter which I have found necessary working from certain environment and versions of python. I suggest trying to run this script normally with the requests library before proceeding to disable ssl with this custom adapter if you get an ssl error.
 from requests import Session
 from requests import adapters
 from urllib3 import poolmanager
