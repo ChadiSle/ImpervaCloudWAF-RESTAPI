8546bc7fb1d8f5faf378f31f0a0c383df5400541
ChadiSle <csleiman21@gmail.com>
2025-01-30 15:28:23 -0500

    Update DeleteSites.py

--- ConfigurationManagement/DeleteSites.py
+++ ConfigurationManagement/DeleteSites.py
@@ -78,6 +78,10 @@ for site_id in site_ids:
 
     api_url = f"https://my.imperva.com/api/prov/v1/sites/delete?site_id={site_id}&ignore_grace_period=true"
 
-    response = ssl_supressed_session().post(api_url, headers=headers, verify=False)
+    # If using an ssl suppresed session, comment out the requests.post() and uncomment the ssl_supressed_session().post() 
+    #response = ssl_supressed_session().post(api_url, headers=headers, verify=False)
+    
+    response = requests.post(api_url, headers=headers, verify=False)
+    
     print(response.status_code)
     print(f"Site ID {site_id} has been deleted")
